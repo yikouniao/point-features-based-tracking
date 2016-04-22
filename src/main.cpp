5e7b2feeab68f24e69e61e9917dc9c72d128f7bb
yikouniao <chinoandtippi@gmail.com>
2016-04-22 21:57:32 +0800

    orb descriptors match

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include "orb.h"
 #include <iostream>
-#include "test-cv.h"
 #include "err.h"
 #include "file.h"
 #include "types.h"
@@ -8,26 +7,37 @@
 
 using namespace std;
 
-#define TEST 1
-
 string f_name0 = "complex building/simu0014.bmp";
 string f_name1 = "complex building/simu0076.bmp";
 
 int main(int argc, char** argv) {
-#if TEST
-  test(f_name0, f_name1);
-#else
-  Mat img = ImgRead(f_name0);
+  Mat img0 = ImgRead(f_name0);
 
   OrbMethod* orbd = new OrbMethod();
-  vector<KeyPoint> keypoints;
-  OrbDescriptors descriptors;
-  orbd->OrbImpl(img, keypoints, descriptors);
+  vector<KeyPoint> keypoints0;
+  OrbDescriptors descriptors0;
+  orbd->OrbImpl(img0, keypoints0, descriptors0);
 
   //MarkKeyPoints(img, keypoints);
   //ImgWrite("../result.bmp", img);
 
+  img0.Release();
+
+  Mat img1 = ImgRead(f_name1);
+
+  vector<KeyPoint> keypoints1;
+  OrbDescriptors descriptors1;
+  orbd->OrbImpl(img1, keypoints1, descriptors1);
+
+  //MarkKeyPoints(img, keypoints);
+  //ImgWrite("../result.bmp", img);
+
+  img1.Release();
+
   delete orbd;
-  img.Release();
-#endif
+
+  std::vector<DescMatch> matches;
+  OrbMatch(descriptors0, descriptors1, matches);
+
+  return 0;
 }
